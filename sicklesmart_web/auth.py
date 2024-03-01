81e16f7eee7f6f46d935b1386558d1bf86bac84b
AyomideKayode <ayomidekay7@gmail.com>
2024-03-01 16:25:13 +0100

    FORM method request in progress.🚧

--- sicklesmart_web/auth.py
+++ sicklesmart_web/auth.py
@@ -3,19 +3,21 @@
 """Auth module for user's authentication.
 """
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-	return render_template("login.html")
+    return render_template("login.html")
+
 
 @auth.route('/logout')
 def logout():
-	return "<p>Log out</p>"
+    return "<p>Log out</p>"
 
-@auth.route('/register')
+
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
-	return render_template("register.html")
+    return render_template("register.html")
