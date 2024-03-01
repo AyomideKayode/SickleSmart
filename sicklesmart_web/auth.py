90f07a1db7081340adfd71a2f3bb85e5eee89a66
AyomideKayode <ayomidekay7@gmail.com>
2024-03-01 18:07:16 +0100

    Setup Flash Messages.🚧

--- sicklesmart_web/auth.py
+++ sicklesmart_web/auth.py
@@ -3,7 +3,7 @@
 """Auth module for user's authentication.
 """
 
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -20,4 +20,27 @@ def logout():
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        age = request.form.get('age')
+        bloodGroup = request.form.get('bloodGroup')
+        genotype = request.form.get('genotype')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters and be valid',
+                  category='error')
+        elif len(name) < 2:
+            flash('Valid name longer that two characters is required',
+                  category='error')
+        elif password1 != password2:
+            flash("Passwords don't match", category='error')
+        elif len(password1) < 8:
+            flash("Password length is too short", category='error')
+        else:
+            # add user to database
+            flash('Sign Up for SickleSmart successful!', category='success')
+
     return render_template("register.html")
