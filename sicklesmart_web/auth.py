924ba66f7ffda84b19170dc0e1268bfc45326312
AyomideKayode <ayomidekay7@gmail.com>
2024-03-01 20:34:39 +0100

    Login & Register POST methods elaborated on and modified so that user data is being stored and users are recognised when they come back to access the web app perks.🚧

--- sicklesmart_web/auth.py
+++ sicklesmart_web/auth.py
@@ -3,13 +3,30 @@
 """Auth module for user's authentication.
 """
 
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in succesfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('You dey wyn me? E no correct jhur', category='error')
+        else:
+            flash('Email does not exist - I no sabi you before', category='error')
+
     return render_template("login.html")
 
 
@@ -29,7 +46,10 @@ def register():
         bloodGroup = request.form.get('bloodGroup')
         genotype = request.form.get('genotype')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Person don use this email before, change am', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters and be valid',
                   category='error')
         elif len(name) < 2:
@@ -41,6 +61,12 @@ def register():
             flash("Password length is too short", category='error')
         else:
             # add user to database
+            new_user = User(email=email, user_name=name,
+                            password=generate_password_hash(password1,
+                                                            method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Sign Up for SickleSmart successful!', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("register.html")
