ab41747abf3de886d81fd2d8d92d4184703656d1
AyomideKayode <ayomidekay7@gmail.com>
2024-03-01 13:34:37 +0100

    Login and Register implementation - HTML, CSS and JavaScript for now.🚧

---
+++ static/scripts/login_register.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const loginButton = document.querySelector('.home-login');
+  const registerButton = document.querySelector(
+    '.home-btn-group button:not(.home-login)'
+  );
+
+  // Add click event listener to login button
+  loginButton.addEventListener('click', function () {
+    // Redirect to the login page
+    window.location.href = 'login.html';
+  });
+
+  // Add click event listener to register button
+  registerButton.addEventListener('click', function () {
+    // Redirect to the register page
+    window.location.href = 'register.html';
+  });
+});
