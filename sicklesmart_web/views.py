956a0d2709b872a5640d1058a443cce620c4c488
AyomideKayode <ayomidekay7@gmail.com>
2024-03-04 23:46:41 +0100

    Implemented the Health Tracking feature. Where logged in users can add/write a short description of how they feel, medications used, hydration levels, etc. I should definitely improve it.🚧🤔

--- sicklesmart_web/views.py
+++ sicklesmart_web/views.py
@@ -4,8 +4,11 @@
 my application. Meaning it has the URLs(roots) defined here.
 """
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+from .models import HealthStatus
+from . import db
+
 
 views = Blueprint('views', __name__)
 
@@ -15,9 +18,21 @@ def home():
     return render_template('index.html', user=current_user)
 
 
-@views.route('/user-logged_in')
+@views.route('/user-logged_in', methods=['GET', 'POST'])
 @login_required
 # to be added to the page route that should be dsiplayed when
 # users have successfully registered or logged in.
 def user_logged_in():
+    if request.method == 'POST':
+        healthStatus = request.form.get('healthStatus')
+
+        if len(healthStatus) < 1:
+            flash('Details🙂, how do you actually feel?', category='error')
+        else:
+            new_status = HealthStatus(symptoms=healthStatus, user_id=current_user.id)
+            db.session.add(new_status)
+            db.session.commit()
+            flash('Health tracked successfully! Remember to drink more water.😁',
+                  category='success')
+
     return render_template('user-logged_in.html', user=current_user)
